eba6b3db1b1d5e5103473ab02aef59f0c2e042a5
e133tc1pher <e133tc1pher@b07a0e73-3d42-0410-ba36-8d2d3503ca73>
2008-01-14 06:18:02 +0000

    Working version, this runs at 125 Mhz, unfortunately unable to make the desired 200 Mhz timing.  This will likely be the last modifification to this, and a refactored version will be started that provides the necessary interfaces and abstractions to be used in a real system.
    git-svn-id: https://fpga-image-registration.googlecode.com/svn/trunk@10 b07a0e73-3d42-0410-ba36-8d2d3503ca73

--- modules/zbt_ram_test/zbt_controller.vhd
+++ modules/zbt_ram_test/zbt_controller.vhd
@@ -62,31 +62,31 @@ END zbt_controller;
 
 ARCHITECTURE Behavioral OF zbt_controller IS
   TYPE   ZBT_STATE IS (IDLE, WRITE_START, WRITE_CMD, WRITE_WAIT, WRITE, READ_START, READ_CMD, READ_WAIT, READ);
-                                                                        -- This is the type that defines the possible states the system can be in.
+                              -- This is the type that defines the possible states the system can be in.
 --  SIGNAL RST                 : std_logic;
-  SIGNAL clk_int, clk_buf, clk_intbuf, clk, clk_0         : std_logic;  -- Delay corrected clock for SRAM
-  SIGNAL cur_state                                        : ZBT_STATE                     := IDLE;  -- Current system state
-  SIGNAL cur_addr                                         : unsigned (1 DOWNTO 0)        := (OTHERS => '0');  --(17 DOWNTO 0) := (OTHERS => '0');
-  SIGNAL sram_flash_we_b_reg                              : std_logic                     := '1';
-  SIGNAL sram_cs_b_reg                                    : std_logic                     := '1';
-  SIGNAL gpio_led_reg                                     : std_logic_vector (7 DOWNTO 0) := (OTHERS => '0');
-  SIGNAL initial_read                                     : std_logic                     := '0';
-                                                                        -- This is 1 if the initial read has occured to set gpio_led_reg
-  SIGNAL sram_flash_d_reg                                 : std_logic_vector (7 DOWNTO 0) := (OTHERS => '0');
-                                                                        -- This is a reg for the used portion of the sram_flash_d_res input, it is used to enable tri-state output on the register
-  SIGNAL sram_flash_d_oe_b                                : std_logic                     := '0';
-                                                                        -- This is a register that determines if the sram_flash_d_reg is connected to the inout port, or floated
-  SIGNAL user_input_enable                                : std_logic                     := '1';  -- This is a trivial way to fix button bouncing, after each command, require that a dedicated button be pressed to allow input to occur again.
+  SIGNAL clk_int, clk_buf, clk_intbuf, clk, clk_0                                                       : std_logic;  -- Delay corrected clock for SRAM
+  SIGNAL cur_state                                                                                      : ZBT_STATE                     := IDLE;  -- Current system state
+  SIGNAL cur_addr                                                                                       : unsigned (1 DOWNTO 0)         := (OTHERS => '0');  --(17 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL sram_flash_we_b_reg                                                                            : std_logic                     := '1';
+  SIGNAL sram_cs_b_reg                                                                                  : std_logic                     := '1';
+  SIGNAL gpio_led_reg                                                                                   : std_logic_vector (7 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL initial_read                                                                                   : std_logic                     := '0';
+                              -- This is 1 if the initial read has occured to set gpio_led_reg
+  SIGNAL sram_flash_d_reg                                                                               : std_logic_vector (7 DOWNTO 0) := (OTHERS => '0');
+                              -- This is a reg for the used portion of the sram_flash_d_res input, it is used to enable tri-state output on the register
+  SIGNAL sram_flash_d_oe_b                                                                              : std_logic                     := '0';
+                              -- This is a register that determines if the sram_flash_d_reg is connected to the inout port, or floated
+  SIGNAL user_input_enable                                                                              : std_logic                     := '1';  -- This is a trivial way to fix button bouncing, after each command, require that a dedicated button be pressed to allow input to occur again.
 -- TODO generate SRAM_CLK from input clock, use it for all processing in this
 -- module;
-  SIGNAL global_reset                                     : std_logic;  -- This combines all of the reset
+  SIGNAL global_reset                                                                                   : std_logic;  -- This combines all of the reset
                                         -- signals (active low)
-  SIGNAL sram_dll_locked, int_dll_locked, startup_dcm_rst : std_logic;
-  SIGNAL gpio_sw_reg : std_logic_vector (4 DOWNTO 0) := (OTHERS => '0');
-  SIGNAL rst_reg : std_logic := '1';
-  SIGNAL fpga_rotary_reg : std_logic_vector (2 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL sram_dll_locked, initial_dll_locked, int_dll_locked, startup_dcm_rst, clk_predcm, clk0_initial : std_logic;
+  SIGNAL gpio_sw_reg                                                                                    : std_logic_vector (4 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL rst_reg                                                                                        : std_logic                     := '1';
+  SIGNAL fpga_rotary_reg                                                                                : std_logic_vector (2 DOWNTO 0) := (OTHERS => '0');
 BEGIN
-  global_reset     <= rst_reg AND sram_dll_locked AND int_dll_locked;
+  global_reset     <= rst_reg AND sram_dll_locked AND initial_dll_locked AND int_dll_locked;
   SRAM_BW_B        <= "0000";
   SRAM_ADV_LD_B    <= '0';
   SRAM_A           <= ("0000000000000000"&std_logic_vector(cur_addr));
@@ -102,10 +102,49 @@ BEGIN
     GENERIC MAP (
       IOSTANDARD => "DEFAULT")
     PORT MAP (
-      O  => clk,                        -- Clock buffer output
+      O  => clk_predcm,                 -- Clock buffer output
       I  => CLK_P,                      -- Diff_p clock buffer input
       IB => CLK_N                       -- Diff_n clock buffer input
       );
+
+  DCM_BASE_initial : DCM_BASE
+    GENERIC MAP (
+      CLKDV_DIVIDE          => 2.0,  -- Divide by: 1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5
+      --   7.0,7.5,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0 or 16.0
+      CLKFX_DIVIDE          => 8,       -- Can be any interger from 1 to 32
+      CLKFX_MULTIPLY        => 5,       -- Can be any integer from 2 to 32
+      CLKIN_DIVIDE_BY_2     => false,  -- TRUE/FALSE to enable CLKIN divide by two feature
+      CLKIN_PERIOD          => 5.0,  -- Specify period of input clock in ns from 1.25 to 1000.00
+      CLKOUT_PHASE_SHIFT    => "NONE",  -- Specify phase shift mode of NONE or FIXED
+      CLK_FEEDBACK          => "1X",    -- Specify clock feedback of NONE or 1X
+      DCM_AUTOCALIBRATION   => true,   -- DCM calibrartion circuitry TRUE/FALSE
+      DCM_PERFORMANCE_MODE  => "MAX_SPEED",  -- Can be MAX_SPEED or MAX_RANGE
+      DESKEW_ADJUST         => "SYSTEM_SYNCHRONOUS",  -- SOURCE_SYNCHRONOUS, SYSTEM_SYNCHRONOUS or
+                                        --   an integer from 0 to 15
+      DFS_FREQUENCY_MODE    => "HIGH",  -- LOW or HIGH frequency mode for frequency synthesis
+      DLL_FREQUENCY_MODE    => "HIGH",  -- LOW, HIGH, or HIGH_SER frequency mode for DLL
+      DUTY_CYCLE_CORRECTION => true,    -- Duty cycle correction, TRUE or FALSE
+      FACTORY_JF            => X"F0F0",  -- FACTORY JF Values Suggested to be set to X"F0F0" 
+      PHASE_SHIFT           => 0,  -- Amount of fixed phase shift from -255 to 1023
+      STARTUP_WAIT          => false)  -- Delay configuration DONE until DCM LOCK, TRUE/FALSE
+    PORT MAP (
+      CLK0   => clk0_initial,           -- 0 degree DCM CLK ouptput
+      --CLK180 => CLK180,     -- 180 degree DCM CLK output
+      --CLK270 => CLK270,     -- 270 degree DCM CLK output
+      --CLK2X => CLK2X,       -- 2X DCM CLK output
+      --CLK2X180 => CLK2X180, -- 2X, 180 degree DCM CLK out
+      --CLK90 => CLK90,       -- 90 degree DCM CLK output
+      --CLKDV => CLKDV,       -- Divided DCM CLK out (CLKDV_DIVIDE)
+      CLKFX  => clk,                    -- DCM CLK synthesis out (M/D)
+      --CLKFX180 => CLKFX180, -- 180 degree CLK synthesis out
+      LOCKED => initial_dll_locked,     -- DCM LOCK status output
+      CLKFB  => clk0_initial,           -- DCM clock feedback
+      CLKIN  => clk_predcm,        -- Clock input (from IBUFG, BUFG or DCM)
+      RST    => '0'                     -- DCM asynchronous reset input
+      );
+
+
+
   BUFGCTRL_inst : BUFGCTRL
     GENERIC MAP (
       INIT_OUT     => 0,      -- Inital value of 0 or 1 after configuration
@@ -136,22 +175,46 @@ BEGIN
       A1  => '1',                       -- Select[1] input
       A2  => '1',                       -- Select[2] input
       A3  => '1',                       -- Select[3] input
-      CLK => clk,                       -- Clock input
+      CLK => clk_predcm,                -- Clock input
       D   => '0'                        -- SRL data input
       );
 
 -------------------------------------------------------------------------------
   --This DDL is used to align the internal FPGA clock
-  CLKDLLHF_internal : CLKDLLHF
+  DCM_BASE_internal : DCM_BASE
     GENERIC MAP (
-      DUTY_CYCLE_CORRECTION => true,    --  Duty cycle correct, TRUE or FALSE
-      FACTORY_JF            => X"C080")  --  FACTORY JF Values
+      CLKDV_DIVIDE          => 2.0,  -- Divide by: 1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5
+      --   7.0,7.5,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0 or 16.0
+      CLKFX_DIVIDE          => 1,       -- Can be any interger from 1 to 32
+      CLKFX_MULTIPLY        => 4,       -- Can be any integer from 2 to 32
+      CLKIN_DIVIDE_BY_2     => false,  -- TRUE/FALSE to enable CLKIN divide by two feature
+      CLKIN_PERIOD          => 8.0,  -- Specify period of input clock in ns from 1.25 to 1000.00
+      CLKOUT_PHASE_SHIFT    => "NONE",  -- Specify phase shift mode of NONE or FIXED
+      CLK_FEEDBACK          => "1X",    -- Specify clock feedback of NONE or 1X
+      DCM_AUTOCALIBRATION   => true,   -- DCM calibrartion circuitry TRUE/FALSE
+      DCM_PERFORMANCE_MODE  => "MAX_SPEED",  -- Can be MAX_SPEED or MAX_RANGE
+      DESKEW_ADJUST         => "SYSTEM_SYNCHRONOUS",  -- SOURCE_SYNCHRONOUS, SYSTEM_SYNCHRONOUS or
+                                        --   an integer from 0 to 15
+      DFS_FREQUENCY_MODE    => "HIGH",  -- LOW or HIGH frequency mode for frequency synthesis
+      DLL_FREQUENCY_MODE    => "HIGH",  -- LOW, HIGH, or HIGH_SER frequency mode for DLL
+      DUTY_CYCLE_CORRECTION => true,    -- Duty cycle correction, TRUE or FALSE
+      FACTORY_JF            => X"F0F0",  -- FACTORY JF Values Suggested to be set to X"F0F0" 
+      PHASE_SHIFT           => 0,  -- Amount of fixed phase shift from -255 to 1023
+      STARTUP_WAIT          => false)  -- Delay configuration DONE until DCM LOCK, TRUE/FALSE
     PORT MAP (
-      CLK0   => clk_int,                -- 0 degree DLL CLK ouptput
-      LOCKED => int_dll_locked,         -- DLL LOCK status output
-      CLKFB  => clk_intbuf,             -- DLL clock feedback
-      CLKIN  => clk_buf,  -- Clock input (from IBUFG, BUFG or DLL)
-      RST    => startup_dcm_rst         -- DLL asynchronous reset input
+      CLK0   => clk_int,                -- 0 degree DCM CLK ouptput
+      --CLK180 => CLK180,     -- 180 degree DCM CLK output
+      --CLK270 => CLK270,     -- 270 degree DCM CLK output
+      --CLK2X => CLK2X,       -- 2X DCM CLK output
+      --CLK2X180 => CLK2X180, -- 2X, 180 degree DCM CLK out
+      --CLK90 => CLK90,       -- 90 degree DCM CLK output
+      --CLKDV => CLKDV,       -- Divided DCM CLK out (CLKDV_DIVIDE)
+      --CLKFX => clk_int,       -- DCM CLK synthesis out (M/D)
+      --CLKFX180 => CLKFX180, -- 180 degree CLK synthesis out
+      LOCKED => int_dll_locked,         -- DCM LOCK status output
+      CLKFB  => clk_intbuf,             -- DCM clock feedback
+      CLKIN  => clk_buf,           -- Clock input (from IBUFG, BUFG or DCM)
+      RST    => startup_dcm_rst         -- DCM asynchronous reset input
       );
   BUFG_inst : BUFG
     PORT MAP (
@@ -163,16 +226,42 @@ BEGIN
 -----------------------------------------------------------------------------
   --This DLL is used to align the input clock to the child sram clock to
   --eliminate delay.
-  CLKDLLHF_sram : CLKDLLHF
+
+
+  DCM_BASE_sram : DCM_BASE
     GENERIC MAP (
-      DUTY_CYCLE_CORRECTION => true,    --  Duty cycle correct, TRUE or FALSE
-      FACTORY_JF            => X"C080")  --  FACTORY JF Values
+      CLKDV_DIVIDE          => 2.0,  -- Divide by: 1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5
+      --   7.0,7.5,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0 or 16.0
+      CLKFX_DIVIDE          => 4,       -- Can be any interger from 1 to 32
+      CLKFX_MULTIPLY        => 3,       -- Can be any integer from 2 to 32
+      CLKIN_DIVIDE_BY_2     => false,  -- TRUE/FALSE to enable CLKIN divide by two feature
+      CLKIN_PERIOD          => 8.0,  -- Specify period of input clock in ns from 1.25 to 1000.00
+      CLKOUT_PHASE_SHIFT    => "NONE",  -- Specify phase shift mode of NONE or FIXED
+      CLK_FEEDBACK          => "1X",    -- Specify clock feedback of NONE or 1X
+      DCM_AUTOCALIBRATION   => true,   -- DCM calibrartion circuitry TRUE/FALSE
+      DCM_PERFORMANCE_MODE  => "MAX_SPEED",  -- Can be MAX_SPEED or MAX_RANGE
+      DESKEW_ADJUST         => "SYSTEM_SYNCHRONOUS",  -- SOURCE_SYNCHRONOUS, SYSTEM_SYNCHRONOUS or
+                                        --   an integer from 0 to 15
+      DFS_FREQUENCY_MODE    => "HIGH",  -- LOW or HIGH frequency mode for frequency synthesis
+      DLL_FREQUENCY_MODE    => "HIGH",  -- LOW, HIGH, or HIGH_SER frequency mode for DLL
+      DUTY_CYCLE_CORRECTION => true,    -- Duty cycle correction, TRUE or FALSE
+      FACTORY_JF            => X"F0F0",  -- FACTORY JF Values Suggested to be set to X"F0F0" 
+      PHASE_SHIFT           => 0,  -- Amount of fixed phase shift from -255 to 1023
+      STARTUP_WAIT          => false)  -- Delay configuration DONE until DCM LOCK, TRUE/FALSE
     PORT MAP (
-      CLK0   => clk_0,                  -- 0 degree DLL CLK ouptput
-      LOCKED => sram_dll_locked,        -- DLL LOCK status output
-      CLKFB  => SRAM_CLK_FB,            -- DLL clock feedback
-      CLKIN  => clk_buf,  -- Clock input (from IBUFG, BUFG or DLL)
-      RST    => startup_dcm_rst         -- DLL asynchronous reset input
+      CLK0   => clk_0,                  -- 0 degree DCM CLK ouptput
+      --CLK180 => CLK180,     -- 180 degree DCM CLK output
+      --CLK270 => CLK270,     -- 270 degree DCM CLK output
+      --CLK2X => CLK2X,       -- 2X DCM CLK output
+      --CLK2X180 => CLK2X180, -- 2X, 180 degree DCM CLK out
+      --CLK90 => CLK90,       -- 90 degree DCM CLK output
+      --CLKDV => CLKDV,       -- Divided DCM CLK out (CLKDV_DIVIDE)
+      --CLKFX => clk_0,       -- DCM CLK synthesis out (M/D)
+      --CLKFX180 => CLKFX180, -- 180 degree CLK synthesis out
+      LOCKED => sram_dll_locked,        -- DCM LOCK status output
+      CLKFB  => SRAM_CLK_FB,            -- DCM clock feedback
+      CLKIN  => clk_buf,           -- Clock input (from IBUFG, BUFG or DCM)
+      RST    => startup_dcm_rst         -- DCM asynchronous reset input
       );
 
 -------------------------------------------------------------------------------  
@@ -197,10 +286,10 @@ BEGIN
   PROCESS (clk_intbuf) IS
   BEGIN  -- PROCESS
     IF clk_intbuf'event AND clk_intbuf = '1' THEN  -- rising clock edge
-      gpio_sw_reg <= GPIO_SW;
+      gpio_sw_reg     <= GPIO_SW;
       fpga_rotary_reg <= FPGA_ROTARY;
-      rst_reg <= RST;
-      
+      rst_reg         <= RST;
+
       IF global_reset = '0' THEN        -- synchronous reset (active low)
         cur_addr          <= (OTHERS => '0');
         initial_read      <= '0';
@@ -242,7 +331,7 @@ BEGIN
                 user_input_enable <= '0';
                 cur_addr          <= cur_addr + 1;  -- Inc
                 cur_state         <= READ_START;
-              ELSIF fpga_rotary_reg(2) = '1' THEN  -- Save
+              ELSIF fpga_rotary_reg(2) = '1' THEN   -- Save
                 user_input_enable <= '0';
                 cur_state         <= WRITE_START;
               ELSIF gpio_sw_reg(0) = '1' THEN

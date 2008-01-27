893b17953e25373621688db68f99f5077c951fcd
e133tc1pher <e133tc1pher@b07a0e73-3d42-0410-ba36-8d2d3503ca73>
2008-01-27 00:22:30 +0000

    This is the RAM manager with a corresponding test sequence, it still has a problem where the first read is not properly coming in, but subsequent reads do.  This seems to point towards the OE handeling, and that will need to be corrected for proper operation.  Some of the comments in the testing state machine are incorrect as the test itself will match the comments in the final version.
    git-svn-id: https://fpga-image-registration.googlecode.com/svn/trunk@13 b07a0e73-3d42-0410-ba36-8d2d3503ca73

--- modules/zbt_ram_manager/zbt_controller.vhd
+++ modules/zbt_ram_manager/zbt_controller.vhd
@@ -16,6 +16,13 @@
 -- Revision 0.01 - File Created
 -- Additional Comments: 
 --
+-------------------------------------------------------------------------------
+-- DOESN'T SUPPORT:  ADV_LD_B=1, CKE_B=1, 
+-- TODO
+-- Fix ADV such that when bursting, it maintains it's mode (so that we still
+-- can accurately say when data is ready)
+-- Use generators to implement delays so that they can be parameterized
+
 ----------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
@@ -28,42 +35,88 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 ENTITY zbt_controller IS
-  PORT (CLK        : IN  std_logic;
-        RST        : IN  std_logic;
-        CLKEN      : IN  std_logic;
-        BW_B       : IN  std_logic_vector (3 DOWNTO 0);
-        WE_B       : IN  std_logic;
-        ADDR       : IN  std_logic_vector (17 DOWNTO 0);
-        DATA       : IN  std_logic_vector (35 DOWNTO 0);
+  PORT (CLK : IN std_logic;
+        RST : IN std_logic;
 
-        DATA_VALID : OUT std_logic;
+        -- Control signals
+        ADV_LD_B        : IN  std_logic;
+        ADDR            : IN  std_logic_vector (17 DOWNTO 0);
+        WE_B            : IN  std_logic;
+        BW_B            : IN  std_logic_vector (3 DOWNTO 0);
+        CKE_B           : IN  std_logic;
+        CS_B            : IN  std_logic;
+        DATA_WRITE      : IN  std_logic_vector (35 DOWNTO 0);
+        DATA_READ       : OUT std_logic_vector(35 DOWNTO 0);
+        DATA_READ_VALID : OUT std_logic;
 
-        -- SRAM Outputs
-        SRAM_CS_B  : OUT std_logic;
-        SRAM_BW_B  : OUT std_logic_vector (3 DOWNTO 0);
-        SRAM_OE_B  : OUT std_logic;
-        SRAM_WE_B  : OUT std_logic;
-        SRAM_ADDR  : OUT std_logic_vector (17 DOWNTO 0);
-        SRAM_DATA  : OUT std_logic_vector (35 DOWNTO 0));
+        -- SRAM Connections
+        SRAM_ADV_LD_B : OUT   std_logic;
+        SRAM_ADDR     : OUT   std_logic_vector (17 DOWNTO 0);
+        SRAM_WE_B     : OUT   std_logic;
+        SRAM_BW_B     : OUT   std_logic_vector (3 DOWNTO 0);
+        SRAM_CKE_B    : OUT   std_logic;
+        SRAM_CS_B     : OUT   std_logic;
+        SRAM_OE_B     : OUT   std_logic;
+        SRAM_DATA     : INOUT std_logic_vector (35 DOWNTO 0));
 END zbt_controller;
 
 ARCHITECTURE Behavioral OF zbt_controller IS
-
+  SIGNAL data_write_delay0, data_write_delay1, data_write_delay2 : std_logic_vector(35 DOWNTO 0);  -- These take the data to be written and apply them 2 clock times after the command started.
+  SIGNAL we_b_delay, cs_b_delay                                  : std_logic_vector(2 DOWNTO 0) := (OTHERS => '1');
+  SIGNAL data_read_valid_reg : std_logic := '0';
 BEGIN
+  SRAM_ADV_LD_B <= ADV_LD_B;
+  SRAM_ADDR     <= ADDR;
+  SRAM_WE_B     <= WE_B;
+  SRAM_BW_B     <= BW_B;
+  SRAM_CKE_B    <= CKE_B;
+  SRAM_CS_B     <= CS_B;
+  DATA_READ     <= SRAM_DATA;
+
+
+  DATA_READ_VALID <= data_read_valid_reg;
+  
 -- purpose: This is the SRAM manager body, it receives data and outputs it with the correct timing to the ZBT ram.  It signals when read data is available (not provided by this module, it is read directly from the RAM,  this module just says when it is valid for reading).
 -- type   : sequential
--- inputs : CLK, RST, CLKEN, BW_B, WE_B, ADDR, DATA
--- outputs: SRAM_CS_B, SRAM_BW_B, SRAM_OE_B, SRAM_WE_B, SRAM_ADDR, SRAM_DATA, READ_DATA_VALID
   PROCESS (CLK) IS
   BEGIN  -- PROCESS
-    IF CLK'event AND CLK = '1' AND CLKEN= '1' THEN     -- rising clock edge
-      IF RST = '1' THEN                 -- synchronous reset (active high)
+    IF CLK'event AND CLK = '1' THEN     -- rising clock edge
+      SRAM_OE_B     <= NOT we_b_delay(0);
 
+       -- Control write data output
+      IF  we_b_delay(0) = '0' AND cs_b_delay(0) = '0' THEN
+        SRAM_DATA <= data_write_delay0;
       ELSE
-        
-    END IF;
-  END IF;
-END PROCESS;
+        SRAM_DATA <= (OTHERS => 'Z');
+      END IF;
 
+      
+      --SRAM_DATA <= data_write_delay0 WHEN we_b_delay(0) = '0' AND cs_b_delay(0) = '0' ELSE (OTHERS => 'Z');
+
+      data_write_delay0 <= DATA_WRITE;
+      data_write_delay1 <= data_write_delay0;
+      data_write_delay2 <= data_write_delay1;
+      
+      IF RST = '1' THEN                 -- synchronous reset (active high)
+        cs_b_delay <= (OTHERS => '1');
+        we_b_delay <= (OTHERS => '1');
+        data_read_valid_reg <= '0';
+        
+      ELSE
+        -- Signify whether the data on the DATA_READ lines is valid
+        IF we_b_delay(0) = '1' AND cs_b_delay(0) = '0' THEN
+          data_read_valid_reg <= '1';
+        ELSE
+          data_read_valid_reg <= '0';  
+        END IF;
+        cs_b_delay(0) <= CS_B;
+        cs_b_delay(1) <= cs_b_delay(0);
+        cs_b_delay(2) <= cs_b_delay(1);
+        we_b_delay(0)     <= WE_B;
+        we_b_delay(1)     <= we_b_delay(0);
+        we_b_delay(2)     <= we_b_delay(1);
+      END IF;
+    END IF;
+  END PROCESS;
 END Behavioral;
 

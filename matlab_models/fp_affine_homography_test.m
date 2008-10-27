0afd62b1233105d05b07d0715ec7bab096241ff5
Brandyn A. White <bwhite@cs.ucf.edu>
2008-10-27 18:08:10 -0400

    Updated matlab models.

--- matlab_models/fp_affine_homography_test.m
+++ matlab_models/fp_affine_homography_test.m
@@ -1,37 +1,58 @@
 % Target error is less than .1 (that is SumSquaredError)
 max_error=0;
 sum_error=0;
-num_iters=100;
+num_iters=500;
 for i=1:num_iters
-    affine_radius=32;
-    trans_radius=1024;
-    coord_radius=1024;
-    A=(rand(2,2)-.5)*2*affine_radius; % Generates a random affine sub matrix[-affine_rad,affine_rad]
-    t=(rand(2,1)-.5)*2*trans_radius; % Generates a random translation vector[-trans_rad,trans_rad]
-    x=rand(2,1)*coord_radius; % Generates a random coord vector[0,coord_rad]
-
+    affine_radius=1;
+    trans_radius=100;
+    coord_radius=500;
+    %affine_radius=64;
+    %trans_radius=1024;
+    %coord_radius=1024;
+    A=(rand(2,2))*affine_radius; % Generates a random affine sub matrix[-affine_rad,affine_rad]
+    t=(rand(2,1))*trans_radius; % Generates a random translation vector[-trans_rad,trans_rad]
+    
+    %A=(rand(2,2)-.5)*2*affine_radius; % Generates a random affine sub matrix[-affine_rad,affine_rad]
+    %t=(rand(2,1)-.5)*2*trans_radius; % Generates a random translation vector[-trans_rad,trans_rad]
+    x=round(rand(2,1)*coord_radius); % Generates a random coord vector[0,coord_rad)
+    for j=1:2
+        if x(j)==coord_radius
+            x(j)=coord_radius - 1;
+        end
+    end
+    A0=A;
+    t0=t;
+    x0=x;
     % Result using double precision floating point
     x_p=fp_affine_homography(A,t,x);
 
     % Result using fixed point
     affine_word=18;
-    affine_whole=5;
-    trans_word=25;
+    affine_whole=6;
+    trans_word=22;
     trans_whole=10;
-    coord_word=25;
+    coord_word=11;
     coord_whole=10;
-    out_word=25;
-    out_whole=18;
+    out_word=20;
+    out_whole=19;
     
-    A=fi(A,1,affine_word,affine_word-affine_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024);
-    t=fi(t,1,trans_word,trans_word-trans_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024);
-    x=fi(x,1,coord_word,coord_word-coord_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024);
+    A=fi(A,1,affine_word,affine_word-affine_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024,'RoundMode','Round');
+    t=fi(t,1,trans_word,trans_word-trans_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024,'RoundMode','Round');
+    x=fi(x,1,coord_word,coord_word-coord_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024,'RoundMode','Round');
     H=[A,t];
     H=[H;0 0 1];
     x_p_fp=fp_affine_homography(A,t,x);
+    x_p_fp=fi(x_p_fp,1,out_word,out_word-out_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024,'RoundMode','Round');% Rounding occurs here
     delx=double(x_p-double(x_p_fp));
     err=sum(delx.*delx);
     max_error=max(double(err),max_error);
+    if max_error == double(err)
+        disp('MaxErr')
+        disp(max_error)
+        disp(double(A0))
+        disp(double(t0))
+        disp(double(x0))
+    end
     sum_error=sum_error+double(err);
 end
 disp(sprintf('AvgErr:%f Max Err:%f',sum_error/num_iters,max_error))
--- matlab_models/fp_convolution_pixel_addressing.m
+++ matlab_models/fp_convolution_pixel_addressing.m
@@ -3,48 +3,36 @@
 % convolution style pixel ordering.  This ordering removes the need to
 % buffer entire lines/columns of image data.
 % From [0,len-1] ordering, mem_addr is [0,height*width-1]
-function [mem_addresses,xy_coords,conv_pos,ti_string]=fp_convolution_pixel_addressing(height,width,conv_height,row_skip,border_size)
+% conv_width is optional, if it is omitted
+function [xy_coords,mem_addresses]=fp_convolution_pixel_addressing(height,width,conv_height,row_skip)
 x=0;
 y=0;
+mem_addr=0;
 xy_coords=[];
 mem_addresses=[];
-conv_pos=[];
 if ~exist('conv_width')
     conv_width=0;
 end
 if ~exist('row_skip')
-    row_skip=0;
-end
-if ~exist('border_size')
-    border_size=0;
+    row_skip=1;
 end
 
-% Display the TI line for unit testing
-width_offset=(conv_height-1)*width-1;
-last_valid_y=height-conv_height-border_size-mod(height-2*border_size-conv_height,1+row_skip);
-new_row_offset=width_offset-2*border_size-row_skip*width;
-ti_string=sprintf('TI:{1,%d,%d,%d,%d,%d,0}',height,width,width_offset,last_valid_y,new_row_offset);
-disp(ti_string)
-
-mem_addr=border_size*width+border_size;
-for i=border_size:row_skip+1:height-conv_height-border_size
-    for j=border_size:width-1-border_size
+for i=0:row_skip:height-conv_height
+    for j=0:width-1
         for k=0:conv_height-1
             x=j;
             y=i+k;
             xy_coords=[xy_coords;x,y];
             mem_addresses=[mem_addresses;mem_addr];
-            conv_pos=[conv_pos;k];
             mem_addr=mem_addr+width;
             tmp_img=zeros(height,width);
             tmp_img(y+1,x+1)=1;
             figure(1);imagesc(tmp_img);
-            pause(.1)
+            pause(.2)
         end
         mem_addr=mem_addr-conv_height*width; % NOTE This multiply is known at synthesis time
         mem_addr=mem_addr+1;
     end
-    mem_addr=mem_addr+2*border_size+row_skip*width;
 end
 
 num_failed=0;

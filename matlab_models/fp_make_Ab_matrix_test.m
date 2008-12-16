777bf44b23456462aa901d902b4aec0eea564ec7
brandyn <brandyn@brandyn-macbook-linux.(none)>
2008-12-16 00:15:23 -0500

    Tested on a wider range of test values including all valid coordinate shifts.

--- matlab_models/fp_make_Ab_matrix_test.m
+++ matlab_models/fp_make_Ab_matrix_test.m
@@ -1,4 +1,7 @@
-coord_bits=10;
-for i=1:100
-    fp_make_Ab_matrix(2*(rand()-.5)*2^(coord_bits-1),2*(rand()-.5)*2^(coord_bits-1),2*(rand()-.5),2*(rand()-.5),2*(rand()-.5),coord_bits-1,1);
+for j=5:10
+    coord_bits=j+1;
+    for i=1:20
+        fp_make_Ab_matrix(fix(4*(rand()-.5)*2^(coord_bits-1))/2,fix(4*(rand()-.5)*2^(coord_bits-1))/2,2*(rand()-.5),2*(rand()-.5),2*(rand()-.5),coord_bits-1,1);
+    end
 end
+

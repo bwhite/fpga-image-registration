7c3e3e584f1cbff5d88132fafea507e1e627f109
brandyn <brandyn@brandyn-macbook-linux.(none)>
2009-02-19 16:48:25 -0500

    Cleaned up most of the modules.

--- matlab_models/fp_derivative_computation_3x3.m
+++ matlab_models/fp_derivative_computation_3x3.m
@@ -7,28 +7,19 @@
 
 % TODO investigate smoothing, and effects of using a 3x3 convolution vs 5x5
 % (test with real data)
-function [Ix,Iy,It]=fp_derivative_computation_3x3(J,x,mask_smooth,output_word,output_whole)
-
-if ~exist('mask_smooth')
-    % Method 1 - 3x3 and 3x3 derivative mask [-1 0 1] with gaussian
-    % smoothing
-    mask_smooth=fspecial('gaussian',[3,1],1);
-end
-
-mask_der=[-1 0 1];%.*fspecial('gaussian',[3,1],1)';
+function [Ix,Iy,It]=fp_derivative_computation_3x3(J,x,output_word,output_whole)
+mask_der=[-1 0 1];
 
 % Temporal Derivative Computation Methods
 % Method 1 - Simple difference of middle pixel values
 It=x-J(2,2);
 
 % Compute J, smoothed in X and Y directions
-J_smooth_y=[J(1,:)+J(2,:)+J(3,:)*mask_smooth(1)];
-J_smooth_x=[J(:,1)+J(:,2)+J(:,3)*mask_smooth(1)];
+J_smooth_y=J(1,:)+J(2,:)+J(3,:);
+J_smooth_x=J(:,1)+J(:,2)+J(:,3);
 
 % Compute derivative in opposing direction
 Ix_presum=J_smooth_y.*mask_der;
 Iy_presum=J_smooth_x.*mask_der';
-%Ix_presum=fi(J_smooth_y.*mask_der,1,output_word,output_word-output_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024);
-%Iy_presum=fi(J_smooth_x.*mask_der',1,output_word,output_word-output_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024);
 Ix=sum(Ix_presum);
 Iy=sum(Iy_presum);
--- matlab_models/fp_make_Ab_matrices.m
+++ matlab_models/fp_make_Ab_matrices.m
@@ -1,105 +1,19 @@
 % This returns the A,b matrices where sum(Ai,i)*x=sum(bi,i), so if these
 % are input for each pixel 
 function [A,b]=fp_make_Ab_matrices(x,y,fx,fy,ft,sx)
+A=ns(zeros(6));
+b=ns(zeros(6,1));
 
-fixed=0;
-if fixed==1
-   A=ns(zeros(6));
-   b=ns(zeros(6,1));
-else
-    % Normalize the coordinates by the nearest power of 2
-    x=x*2^(-sx);
-    y=y*2^(-sx);
-
-    % Pass 1
-    fx2=fx.*fx;
-    fxft=fx.*ft;
-    fxfy=fx.*fy;
-    fy2=fy.*fy;
-    fyft=fy.*ft;
-    x2=x.*x;
-    y2=y.*y;
-    xy=x.*y;
-
-    % Pass 2
-    fx2_t_x=fx2.*x;
-    fx2_t_y=fx2.*y;
-    fx2_t_x2=fx2.*x2;
-    fx2_t_y2=fx2.*y2;
-    fx2_t_xy=fx2.*xy;
-
-    fy2_t_x=fy2.*x;
-    fy2_t_y=fy2.*y;
-    fy2_t_x2=fy2.*x2;
-    fy2_t_y2=fy2.*y2;
-    fy2_t_xy=fy2.*xy;
-
-    fxfy_t_x=fxfy.*x;
-    fxfy_t_y=fxfy.*y;
-    fxfy_t_x2=fxfy.*x2;
-    fxfy_t_y2=fxfy.*y2;
-    fxfy_t_xy=fxfy.*xy;
-
-    fxft_t_x=fxft.*x;
-    fyft_t_x=fyft.*x;
-    fxft_t_y=fxft.*y;
-    fyft_t_y=fyft.*y;
-    sf=2^-sx;
-   A=zeros(6);
-   b=zeros(6,1);
+% Overall FP format for each A matrix 1:11:20 (the summation will have to
+% be larger)
+lx=length(x);
+for i=1:lx
+    disp(sprintf('%d/%d',i,lx))
+    [tA,tb]=fp_make_Ab_matrix(x(i),y(i),fx(i),fy(i),ft(i),sx);
+    A=ns(A)+ns(tA);
+    b=ns(b)+ns(tb);
 end
-% Overall FP format for each A matrix 1:11:20 (the summation will have to be larger)
-maxa=0;
-maxA=zeros(6,1);
-
-all_vec=[];
-for i=1:length(x)
-    if fixed==1
-      [tA,tb]=fp_make_Ab_matrix(x(i),y(i),fx(i),fy(i),ft(i),sx);
-        
-       maxA=max(abs([double(tA),double(maxA),double(tb)]'))';
-        all_vec=[all_vec;tA(:);tb(:)];
-        A=ns(A)+ns(tA);
-        b=ns(b)+ns(tb);
-    else
-        tA=[sf*fx2(i), fx2_t_x(i), fx2_t_y(i), sf*fxfy(i), fxfy_t_x(i), fxfy_t_y(i);
-            sf*fx2_t_x(i), fx2_t_x2(i), fx2_t_xy(i), sf*fxfy_t_x(i), fxfy_t_x2(i), fxfy_t_xy(i);
-            sf*fx2_t_y(i), fx2_t_xy(i), fx2_t_y2(i), sf*fxfy_t_y(i), fxfy_t_xy(i), fxfy_t_y2(i);
-            sf*fxfy(i), fxfy_t_x(i), fxfy_t_y(i), sf*fy2(i), fy2_t_x(i), fy2_t_y(i);
-            sf*fxfy_t_x(i), fxfy_t_x2(i), fxfy_t_xy(i), sf*fy2_t_x(i), fy2_t_x2(i), fy2_t_xy(i);
-            sf*fxfy_t_y(i), fxfy_t_xy(i), fxfy_t_y2(i), sf*fy2_t_y(i), fy2_t_xy(i), fy2_t_y2(i)];
-        tb=[sf*fxft(i);
-            sf*fxft_t_x(i);
-            sf*fxft_t_y(i);
-            sf*fyft(i);
-            sf*fyft_t_x(i);
-            sf*fyft_t_y(i)];
-             maxA=max(abs([double(tA),double(maxA),double(tb)]'))';
-        A=A+tA;
-        b=b+tb;
-    end
-   if fixed==0
-       maxa=max(abs([maxa;tA(:);tb(:)]));
-   end
-end
-if fixed==1
-    b=double(-b);
-    A=double(A);
-    disp('Max Row Values')
-    disp(maxA)
-else
-    %disp('Max Row Values')
-    disp(maxA)
-    cl=clock();
-    save(sprintf('%f-%f-%f.mat',cl(4),cl(5),cl(6)),'A','b');
-    %disp(sprintf('MAXta:%f',maxa))
-    %disp(sprintf('MAXA:%f',max([A(:);b(:)])))
-    b=-b;
-end
-disp('A')
-disp(double(A))
-disp('b')
-disp(double(b))
+b=-b;
 
 function val=ns(x)
 % Overall FP format for each A matrix 1:11:20 (the summation will have to
@@ -108,6 +22,5 @@ coord_word=38;
 coord_whole=6;
 val=fi(x,1,coord_word,coord_word-coord_whole-1,'MaxProductWordLength',1024,'MaxSumWordLength',1024,'RoundMode','Round','SumMode','KeepLSB','SumWordLength',38);
 
-
 function absnormal(x)
-assert(sum(abs(x)>=                 1)==0)
+assert(sum(abs(x)>= 1)==0)
